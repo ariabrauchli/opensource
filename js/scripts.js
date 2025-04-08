2cd626f7b6c7c506135b785c9fb0c25d5cc0a4d2
ariabrauchli <42819915+ariabrauchli@users.noreply.github.com>
2025-04-07 18:10:25 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-
+(function($) {
+	$(document).ready(function() {
+	    $('.tablink').on('click', function(
+        var tabID = $(this).attr('id');
+        $('#wrapper .container').hide();
+        $('#wrapper .container#' + tabID).show();
+      )};
+	});
+})(jQuery);
